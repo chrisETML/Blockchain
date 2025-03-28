4e5901ea0ca7b611075b470b6c93588e76ee2d45
chrisETML <pu36ylg@eduvaud.ch>
2025-03-28 08:15:58 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Entreprise : ETML
+Auteur : Christopher Ristic 
+Date : 28.03.2025
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blockchain
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Programme pour la blockchain
+        /// </summary>
+        static void Main()
+        {
+        }
+    }
+}
